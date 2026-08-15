db1c419f788a34050f722b055be9a0a313bda9bc
Sai Krishna Mohan <m.saikrishna.msk@gmail.com>
2026-08-15 18:22:56 -0400

    venture into async rust

--- healthcheck/src/main.rs
+++ healthcheck/src/main.rs
@@ -1,3 +1,28 @@
+use std::time::Instant;
+
+use trpl;
+#[derive(Debug)]
+pub struct Report {
+    pub url: String,
+    pub bytes: usize,
+    pub ms: u128,
+}
+
+async fn check(url: &str) -> Report {
+    let start = Instant::now();
+    let response = trpl::get(url).await;
+    let body = response.text().await;
+
+    return Report {
+        url: url.to_string(),
+        bytes: body.len(),
+        ms: start.elapsed().as_millis(),
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    trpl::block_on(async {
+        let report = check("https://www.rust-lang.org").await;
+        println!("{:?}", report);
+    });
 }
